883732feb03866cb5f41b3908af53bd43efd9ba1
Rajan <rajanj448@student.sfit.ac.in>
2024-09-16 22:56:31 +0530

    Get User Info

--- src/lib/server/get-user-info.ts
+++ src/lib/server/get-user-info.ts
@@ -1,3 +1,13 @@
-export const getUserInfo=async()=>{
-    
+import "server-only";
+
+import { User } from "@prisma/client";
+import { prisma } from "../prisma/client";
+
+export const getUserInfo=async(userId:User['id'])=>{
+    return prisma.user.findUnique({
+        where:{
+            id:userId
+        }
+    })
+
 }
