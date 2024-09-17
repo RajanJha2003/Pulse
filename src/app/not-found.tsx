c94bb01518a108d43ab4c6d0c3c8ddfcab6a37a9
Rajan <rajanj448@student.sfit.ac.in>
2024-09-17 11:31:38 +0530

    Not Found Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { NotFound } from '@/components/Illustrations'
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div className='grid h-full place-items-center'>
+        <NotFound>
+            We didn&apos;t find what you were looking for.
+        </NotFound>
+
+    </div>
+  )
+}
+
+export default NotFoundPage
