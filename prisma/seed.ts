2b74c4ef1c21a05d64bae0d00e6a02a161ca3549
Rajan <rajanj448@student.sfit.ac.in>
2024-09-18 17:25:53 +0530

    seeding complted

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,89 +1,163 @@
-import { PrismaClient } from "@prisma/client";
-import colors from "tailwindcss/colors";
-import cuid from 'cuid'
 import { faker } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
+import cuid from "cuid";
+import colors from "tailwindcss/colors";
 
-const myUserId="kp_ac62cbf366c7400d88878759686931cf";
+const prisma = new PrismaClient();
 
+// NOTE: IMPORTANT to replace with an user id registered in Kinde
+const myUserId = "kp_4fc8a0fbaaf04a0abbeac48eaa501595";
 
-const prisma =new PrismaClient();
-const runSeed=async()=>{
+const runSeed = async () => {
+  await resetDB();
 
-    await resetDB();
+  process.stdout.write("Generating users...");
+  const myUser = await prisma.user.create({
+    data: {
+      id: myUserId,
+      displayName: "Alice",
+      email: "yaroyac242@biscoine.com",
+      color: colors.red["500"],
+    },
+  });
 
-    process.stdout.write("Generating users");
+  const otherUsers = await prisma.user.createManyAndReturn({
+    data: Array.from({ length: 10 }).map(() => ({
+      id: cuid(),
+      email: faker.internet.email(),
+      displayName: faker.internet.displayName(),
+      createdAt: faker.date.recent(),
+      color: faker.helpers.arrayElement([
+        colors.green[500],
+        colors.red[500],
+        colors.pink[500],
+        colors.purple[500],
+        colors.yellow[500],
+        colors.emerald[500],
+      ]),
+    })),
+  });
 
-    const myUser=await prisma.user.create({
-        data:{
-            id:myUserId,
-            displayName:"Rajan",
-            email:"rajanj448@student.sfit.ac.in",
-            color:colors.red["500"]
+  printCheckMark();
 
-        }
-    })
+  process.stdout.write("Generating events...");
+  const event = await prisma.event.create({
+    data: {
+      ownerId: myUser.id,
+      displayName: faker.commerce.productName(),
+      shortDescription: faker.lorem.sentence(),
+      slug: faker.lorem.slug(),
+      createdAt: faker.date.recent(),
+      participants: {
+        createMany: {
+          data: Array.from({ length: 10 }).map((_, index) => ({
+            userId: otherUsers[index % otherUsers.length].id,
+          })),
+        },
+      },
+    },
+  });
+  printCheckMark();
 
-    const otherUsers=await prisma.user.createManyAndReturn({
-        data:Array.from({length:10}).map(()=>({
-            id:cuid(),
-            email:faker.internet.email(),
-            displayName:faker.internet.displayName(),
-            createdAt:faker.date.recent(),
-            color:faker.helpers.arrayElement([
-                colors.green[500],
-                colors.red[500],
-                colors.pink[500],
-                colors.purple[500],
-                colors.yellow[500],
-                colors.emerald[500],
+  process.stdout.write("Generating questions...");
+  for (let i = 0; i < 30; i++) {
+    await prisma.question.create({
+      data: {
+        eventId: event.id,
+        authorId: otherUsers[i % otherUsers.length].id,
+        body: faker.lorem.paragraph(),
+        isPinned: faker.datatype.boolean({ probability: 0.2 }),
+        isResolved: faker.datatype.boolean({ probability: 0.25 }),
+        createdAt: faker.date.recent(),
+        upvotes: {
+          createMany: {
+            data: Array.from({
+              length: faker.number.int({
+                min: 0,
+                max: 8,
+              }),
+            }).map((_, index) => ({
+              authorId: otherUsers[index % otherUsers.length].id,
+            })),
+          },
+        },
+      },
+    });
+  }
+  printCheckMark();
 
-            ])
-        }))
-    })
+  process.stdout.write("Generating polls...");
+  const polls = await prisma.poll.createManyAndReturn({
+    data: [
+      {
+        eventId: event.id,
+        body: faker.lorem.paragraph(),
+        createdAt: faker.date.recent(),
+        isLive: true,
+      },
+      {
+        eventId: event.id,
+        body: faker.lorem.paragraph(),
+        createdAt: faker.date.recent(),
+        isLive: false,
+      },
+    ],
+  });
 
-   
+  // generate the options and votes for the polls
+  const optionVotes = [
+    otherUsers.slice(0, 4).map((usr) => usr.id), // first option
+    otherUsers.slice(4, 6).map((usr) => usr.id), // second option
+    otherUsers.slice(6, 10).map((usr) => usr.id), // third option
+    [], // fourth option
+  ] as const;
 
-    printCheckMark();
+  for (const poll of polls) {
+    // option per poll
+    for (let i = 0; i < 4; i++) {
+      await prisma.pollOption.create({
+        data: {
+          pollId: poll.id,
+          body: faker.lorem.word(3),
+          index: i,
+          votes: {
+            createMany: {
+              data: Array.from({ length: optionVotes[i].length }).map(
+                (_, userIndex) => ({
+                  authorId: optionVotes[i][userIndex],
+                  pollId: poll.id,
+                })
+              ),
+            },
+          },
+        },
+      });
+    }
+  }
+  printCheckMark();
 
-    process.stdout.write("Generating events...");
+  console.log("Seeding complete 🎉");
+};
 
-    const event=await prisma.event.create({
-        data:{
-            ownerId:myUser.id,
-            displayName:faker.commerce.productName(),
-            shortDescription:faker.lorem.sentence(),
-            slug:faker.lorem.slug(),
-            createdAt:faker.date.recent(),
-            participants:{
-                createMany:{
-                    data:Array.from({length:10}).map((_,index)=>({
-                        userId:otherUsers[index % otherUsers.length].id
-                    }))
-                }
-            }
-        }
-    })
+const resetDB = async () => {
+  process.stdout.write("Resetting database...");
 
-    printCheckMark();
+  await prisma.notification.deleteMany();
+  await prisma.event.deleteMany();
+  await prisma.poll.deleteMany();
+  await prisma.question.deleteMany();
+  await prisma.user.deleteMany();
 
-
-}
-
-const resetDB=async()=>{
-    process.stdout.write("Resetting database...");
-
-    await prisma.user.deleteMany();
-    await prisma.event.deleteMany();
-    await prisma.poll.deleteMany();
-    await prisma.question.deleteMany();
-    await prisma.notification.deleteMany();
-
-    printCheckMark();
-
-
-
-}
+  printCheckMark();
+};
 
 const printCheckMark = () => process.stdout.write("✅\n");
 
-runSeed().then(()=>console.log("FInidhed"));
+runSeed()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => async () => {
+    await prisma.$disconnect();
+  });
