74dc99670db3bb25964ae08599b9af93575e4367
Rajan <rajanj448@student.sfit.ac.in>
2024-09-21 14:00:21 +0530

    Event Card

--- src/app/(auth)/dashboard/(my-events)/page.tsx
+++ src/app/(auth)/dashboard/(my-events)/page.tsx
@@ -1,3 +1,4 @@
+import { UserEventsList } from '@/components/EventsList';
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { getUserEvents } from '@/lib/server/get-user-events';
 import React from 'react'
@@ -9,7 +10,9 @@ const MyEventsPage = async() => {
     <ScrollArea className='w-full h-full px-4 py-2'>
         <h2 className='text-2xl font-bold mb-2 mt-4 ml-4'>Your Events</h2>
         <div className='h-full grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3'>
-
+          
+      
+        <UserEventsList initialEvents={initialEvents} />
 
         </div>
     </ScrollArea>
---
+++ src/components/EventsList.tsx
@@ -0,0 +1,14 @@
+import { EventDetail } from "@/lib/prisma/validators/event-validator"
+import EventCard from "./EventCard"
+
+type Props={
+    initialEvents:EventDetail[]
+}
+
+export const UserEventsList=({initialEvents}:Props)=>{
+    return initialEvents.map((event)=>(
+        <EventCard key={event.id} event={event} className="h-36" />
+        
+    ))
+
+}
