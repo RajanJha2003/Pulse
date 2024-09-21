673d7ff1e33407b0755275e38287522cae2aaf59
Rajan <rajanj448@student.sfit.ac.in>
2024-09-21 23:35:54 +0530

    Add Event

--- src/app/(auth)/dashboard/(my-events)/page.tsx
+++ src/app/(auth)/dashboard/(my-events)/page.tsx
@@ -1,6 +1,7 @@
 import { UserEventsList } from '@/components/EventsList';
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { getUserEvents } from '@/lib/server/get-user-events';
+import { Plus } from 'lucide-react';
 import React from 'react'
 
 const MyEventsPage = async() => {
@@ -12,7 +13,13 @@ const MyEventsPage = async() => {
        
           
       
-       <div>
+       <div className='h-full grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3'>
+       <button className="h-24 md:h-36 bg-white/50 border-[3px] border-spacing-4 border-dashed grid place-items-center group">
+            <span className="inline-flex flex-col items-center gap-x-1 font-medium group-hover:text-blue-600 transition-colors duration-150">
+              <Plus className="w-5 h-5" />
+              <span>New Event</span>
+            </span>
+          </button>
        <UserEventsList initialEvents={initialEvents} />
        </div>
 
